fe1963264ee289b79a09612adb04aa38527f9c81
shrisha <shrisha_sathish@impetusanalytics.com>
2021-10-05 16:52:06 +0530

    UPDATE

--- routes/update.js
+++ routes/update.js
@@ -3,9 +3,11 @@
 module.exports= function(app,db){
 
     app.get('/update',(req,res)=>{
-
+  
         // db.query('UPDATE  alertbox state="B" WHERE alert_id = ?', req.query.id,(e,data1) =>{
-            db.query('UPDATE  alertbox set state="B" WHERE  alert_id IN (?)', [req.query.id],(e,data1) =>{ 
+            var split = req.query.id.split(',')
+            
+            db.query('UPDATE  alertbox set state="B" WHERE  alert_id IN (?)', [split],(e,data1) =>{ 
                 
             
                 res.send(data1) 
